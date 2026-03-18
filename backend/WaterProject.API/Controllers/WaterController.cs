28d5c97d9db9d1983457f6b0dac90280e5280d46
colelatour <clatour0@byu.edu>
2026-03-18 12:27:39 -0600

    Initial Commit

--- backend/WaterProject.API/Controllers/WaterController.cs
+++ backend/WaterProject.API/Controllers/WaterController.cs
@@ -13,9 +13,22 @@ public class WaterController : ControllerBase
     public WaterController(WaterDbContext temp) => _waterContext = temp;
 
     [HttpGet("AllProjects")]
-    public IEnumerable<Project> GetProjects()
+    public IActionResult GetProjects(int pageSize = 10, int pageNum = 1)
     {
-        return _waterContext.Projects.ToList();
+        var something = _waterContext.Projects
+        .Skip((pageNum-1) * pageSize)
+        .Take(pageSize)
+        .ToList();
+
+        var totalNumProjects = _waterContext.Projects.Count();
+
+        var someObject = new
+        {
+            Projects = something,
+            TotalNumProjects = totalNumProjects
+        };
+        
+        return Ok(someObject);
     }
 
     [HttpGet("FunctionalProjects")]
--- backend/WaterProject.API/obj/Debug/net10.0/WaterProject.API.AssemblyInfo.cs
+++ backend/WaterProject.API/obj/Debug/net10.0/WaterProject.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5309d9921a2535ffd4cba243d00038d2d86ec4aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WaterProject.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
